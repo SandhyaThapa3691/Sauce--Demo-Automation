8d2b3b8bc115833423cced88440cf544017b0c47
SandhyaThapa3691 <thapasandhya1301@gmail.com>
2026-01-21 22:35:47 +0545

    first commit

---
+++ tests/test_data.py
@@ -0,0 +1,17 @@
+login_search_data = [
+    {
+        "email": "thapasandhya1301@gmail.com",
+        "password": "Sandhya@1301",
+        "search_term": "grey jacket"
+    },
+    {
+        "email": "thapasandhya1301@gmail.com",
+        "password": "Sandhya@1301",
+        "search_term": "noir jacket"
+    },
+    {
+        "email": "thapasandhya1301@gmail.com",
+        "password": "Sandhya@1301",
+        "search_term": "Striped top"
+    }
+]
