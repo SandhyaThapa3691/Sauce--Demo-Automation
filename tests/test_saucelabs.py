1185dd1578f2462da4d1e9baddf0d97e4f6f123b
SandhyaThapa3691 <thapasandhya1301@gmail.com>
2026-01-22 14:12:45 +0545

    jenkins

--- tests/test_saucelabs.py
+++ tests/test_saucelabs.py
@@ -17,10 +17,10 @@ class SauceLabsTest:
         chrome_options.add_argument("--start-maximized")
 
 
-
         chrome_options.add_argument("--headless=new")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
+
 
 
         service = Service(r"C:\Users\prett\Documents\chromedriver-win32")
