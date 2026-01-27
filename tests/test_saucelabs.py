dee32a17feb176af4ddb4ad00502003d7252bd31
SandhyaThapa3691 <thapasandhya1301@gmail.com>
2026-01-27 21:57:35 +0545

    Jenkins updated

--- tests/test_saucelabs.py
+++ tests/test_saucelabs.py
@@ -17,9 +17,7 @@ class SauceLabsTest:
         chrome_options.add_argument("--start-maximized")
 
 
-        chrome_options.add_argument("--headless=new")
-        chrome_options.add_argument("--no-sandbox")
-        chrome_options.add_argument("--disable-dev-shm-usage")
+
 
 
 
